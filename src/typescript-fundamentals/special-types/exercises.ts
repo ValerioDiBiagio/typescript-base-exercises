07e7007fa3369fd38e7d9dd85b59ff8b4505016d
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-07-04 10:37:46 +0200

    add: extend any type exercise to include variable assignment with an array

--- src/typescript-fundamentals/special-types/exercises.ts
+++ src/typescript-fundamentals/special-types/exercises.ts
@@ -8,3 +8,14 @@ console.log(val);
 
 val = true;
 console.log(val);
+
+// Dichiara una variabile dati di tipo any. Assegnale un numero, poi una stringa, poi un array.
+
+let dati: any = 4;
+console.log(dati);
+
+dati = "Come stai?";
+console.log(dati);
+
+dati = ["mela", "arancia", "pera"];
+console.log(dati);
