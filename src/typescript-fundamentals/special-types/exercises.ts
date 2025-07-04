19762be603ca87c3f5c5a4ada918a49683a0dadc
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-07-04 10:30:22 +0200

    add: add special types exercise demonstrating variable assignment with any type

---
+++ src/typescript-fundamentals/special-types/exercises.ts
@@ -0,0 +1,10 @@
+// Dichiara una variabile val di tipo any e assegnale un numero, poi una stringa, poi un boolean.
+
+let val: any = 6;
+console.log(val);
+
+val = "Ciao";
+console.log(val);
+
+val = true;
+console.log(val);
