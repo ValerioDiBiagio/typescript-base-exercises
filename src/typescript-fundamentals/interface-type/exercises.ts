ba2b1e6d7623493188c9f4096ed564514ae8e68f
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-07-07 16:16:37 +0200

    refactor: add interface type for Product with properties name and price; create and log a product instance

---
+++ src/typescript-fundamentals/interface-type/exercises.ts
@@ -0,0 +1,15 @@
+// Crea un type chiamato Prodotto con queste proprietà:
+
+// nome
+
+// prezzo
+
+// Crea una variabile di tipo Prodotto e stampala.
+
+type prodotto = {
+    nome: string,
+    prezzo: number
+}
+
+const p: prodotto = { nome: "Monitor", prezzo: 250 }
+console.log(p);
