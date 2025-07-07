a28c0a414f75d7041b115ccf84231b3b5fe85b82
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-07-07 10:34:28 +0200

    add: create exercises for array and tuple manipulation

---
+++ src/typescript-fundamentals/array-tuple-obj/exercises.ts
@@ -0,0 +1,26 @@
+// Crea un array di numeri
+// Crea un array contenente i numeri da 1 a 5 e stampa ogni numero presente nell'array.
+
+const numeri: number[] = [1, 2, 3, 4, 5];
+numeri.forEach(numero => console.log(numero));
+
+// Dato l’array [1, 2, 3, 4, 5, 6], crea una funzione che restituisca solo i numeri pari.
+// Somma tutti i numeri dell’array [3, 7, 2, 8, 13].
+
+
+
+
+
+
+
+
+// Crea una tupla con nome ed età e stampa i valori.
+
+
+
+
+// Crea una tupla che rappresenta un libro con titolo, autore, e anno.
+
+
+
+// Scrivi una funzione che prende una tupla [nome, età] e stampa una frase.
