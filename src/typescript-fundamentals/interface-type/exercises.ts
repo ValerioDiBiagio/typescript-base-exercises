a8806a09fae5662d25f6b0370e6869853f259b05
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-07-07 16:23:51 +0200

    refactor: correct type name from 'prodotto' to 'Prodotto' and ensure consistent casing; add interface for 'Persona' with properties nome and eta

--- src/typescript-fundamentals/interface-type/exercises.ts
+++ src/typescript-fundamentals/interface-type/exercises.ts
@@ -6,10 +6,26 @@
 
 // Crea una variabile di tipo Prodotto e stampala.
 
-type prodotto = {
+type Prodotto = {
     nome: string,
     prezzo: number
 }
 
-const p: prodotto = { nome: "Monitor", prezzo: 250 }
+const p: Prodotto = { nome: "Monitor", prezzo: 250 }
 console.log(p);
+
+// Crea un’interface chiamata Persona con:
+
+// nome:
+
+// eta:
+
+// Crea una variabile e stampala con una frase.
+
+interface Persona {
+    nome: string,
+    eta: number
+}
+
+const persona: Persona = { nome: "Mario", eta: 26 }
+console.log(`Sono ${persona.nome} e ho ${persona.eta} anni`);
