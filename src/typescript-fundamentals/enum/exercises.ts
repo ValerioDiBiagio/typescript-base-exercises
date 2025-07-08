13f73e9de3b59251dbe09f7b95fc438e051f2ea0
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-07-08 17:38:10 +0200

    Refine user roles with string enum

--- src/typescript-fundamentals/enum/exercises.ts
+++ src/typescript-fundamentals/enum/exercises.ts
@@ -6,13 +6,13 @@
 
 {
     enum Giorno {
-        Lunedi,
-        Martedi,
-        Mercoledi,
-        Giovedi,
-        Venerdi,
-        Sabato,
-        Domenica
+        Lunedi = "Lunedi",
+        Martedi = "Martedi",
+        Mercoledi = "Mercoledi",
+        Giovedi = "Giovedi",
+        Venerdi = "Venerdi",
+        Sabato = "Sabato",
+        Domenica = "Domenica"
     }
 
     console.log(Giorno.Venerdi);
@@ -57,21 +57,21 @@
 
 {
     enum StatoOrdine {
-        IN_ATTESA,
-        SPEDITO,
-        CONSEGNATO
+        IN_ATTESA = "In attesa",
+        SPEDITO = "Spedito",
+        CONSEGNATO = "Consegnato"
     }
 
     function stato(ordine: StatoOrdine): void {
         switch (ordine) {
             case StatoOrdine.IN_ATTESA:
-                console.log(`L'ordine è ${StatoOrdine[ordine]}`);
+                console.log(`L'ordine è ${ordine}`);
                 break;
             case StatoOrdine.SPEDITO:
-                console.log(`L'ordine è stato ${StatoOrdine[ordine]}`);
+                console.log(`L'ordine è stato ${ordine}`);
                 break;
             case StatoOrdine.CONSEGNATO:
-                console.log(`L'ordine è stato ${StatoOrdine[ordine]}`);
+                console.log(`L'ordine è stato ${ordine}`);
                 break;
             default:
                 console.log("Stato ordine sconosciuto.");
@@ -84,13 +84,36 @@
     stato(StatoOrdine.CONSEGNATO);
 }
 
-
-
 // Crea un enum Ruolo con tre ruoli: Studente, Docente, Amministratore.
-// Scrivi una funzione descriviRuolo(r: Ruolo) che usi uno switch per stampare una descrizione del ruolo.
+// Scrivi una funzione descriviRuolo(r: Ruolo) che usi uno switch per stampare una descrizione di ogni ruolo.
+
+{
+    enum Ruolo {
+        Studente = "Studente",
+        Docente = "Docente",
+        Amministratore = "Amministatore"
+    }
 
 
+    function descriviRuolo(r: Ruolo): void {
+        switch (r) {
+            case Ruolo.Studente:
+                console.log("Lo studente ha il compito di studiare");
+                break;
+            case Ruolo.Docente:
+                console.log("Il docente ha il compito di insegnare");
+                break;
+            case Ruolo.Amministratore:
+                console.log("L'amministratore ha il compito di amministare");
+                break;
+        }
+    }
 
+    descriviRuolo(Ruolo.Studente);
+    descriviRuolo(Ruolo.Docente);
+    descriviRuolo(Ruolo.Amministratore);
+
+}
 
 // Crea un enum stringa Direzione con: "nord", "sud", "est", "ovest".
 // Scrivi una funzione muovi(d: Direzione) che stampi: "Vai a nord", ecc.
