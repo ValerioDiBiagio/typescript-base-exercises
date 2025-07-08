b626fc80e101cfbd3a43b03c3ea5294243a2f74d
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-07-08 09:51:49 +0200

    refactor: add typed functions for sum and greeting; implement age check and name printing functions

---
+++ src/typescript-fundamentals/typed-functions/exercises.ts
@@ -0,0 +1,25 @@
+// Scrivi una funzione somma che accetta due numeri e restituisce la loro somma.
+
+
+{
+    function somma(num1: number, num2: number): number {
+        return num1 + num2
+    }
+
+    console.log(somma(5, 5));
+}
+
+// Scrivi una funzione saluta che prende un nome e ritorna una frase tipo: "Ciao, Mario!".
+
+
+
+// Scrivi una funzione èMaggiorenne che prende un'età e restituisce true se è maggiore o uguale a 18, altrimenti false.
+
+
+
+// Scrivi una funzione stampaNomi che accetta un array di stringhe e stampa ogni nome.
+
+
+
+
+// Scrivi una funzione descriviPersona che prende un oggetto { nome: string; eta: number } e ritorna una stringa descrittiva.
